cbac9424e2f416618ab038c96e5fc9d89d67833c
guojie <2874267468@qq.com>
2018-07-04 14:51:51 +0800

    初始化springBoot 项目

---
+++ src/main/java/com/ctban/swallow/SwallowSpringbootdemoApplication.java
@@ -0,0 +1,12 @@
+package com.ctban.swallow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwallowSpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwallowSpringbootdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ctban/swallow/SwallowSpringbootdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ctban.swallow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwallowSpringbootdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
